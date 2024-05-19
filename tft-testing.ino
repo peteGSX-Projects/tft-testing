29b371285e41fb1b18ed4d83944643b1424d6d10
peteGSX <peteracole@outlook.com.au>
2024-05-20 05:28:45 +1000

    OLED testing

--- tft-testing.ino
+++ tft-testing.ino
@@ -1,24 +1,76 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
-#include <MCUFRIEND_kbv.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_SH110X.h>
 #include <Fonts/FreeMono12pt7b.h>
+#include <Fonts/FreeMono18pt7b.h>
+#include <Fonts/FreeMono24pt7b.h>
+#include <Fonts/FreeMono9pt7b.h>
+#include <Fonts/FreeSans12pt7b.h>
+#include <Fonts/FreeSans18pt7b.h>
+#include <Fonts/FreeSans24pt7b.h>
+#include <Fonts/FreeSans9pt7b.h>
 
-MCUFRIEND_kbv tft;
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define TEXT_SIZE 1
+#define ROTATION 0
+#define TEXT_FONT &FreeMono9pt7b
+#define OLED_ADDRESS 0x3C
+
+#define SCREEN_TYPE OLED_SSD1306
+
+#if SCREEN_TYPE == OLED_SSD1306
+Adafruit_SSD1306 oled = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+#elif SCREEN_TYPE == OLED_SH1106
+Adafruit_SH1106G oled = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+#endif
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(F("Testing"));
-  uint16_t screenId = tft.readID();
-  tft.begin(screenId);
-  tft.setTextSize(1);
-  tft.setRotation(1);
-  const GFXfont *gfxFont = &FreeMono12pt7b;
-  tft.setFont(gfxFont);
+  Serial.println(F("OLED Testing"));
+  const GFXfont *gfxFont = TEXT_FONT;
+#if SCREEN_TYPE == OLED_SSD1306
+  oled.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
+#elif SCREEN_TYPE == OLED_SH1106
+  oled.begin(OLED_ADDRESS);
+#endif
+  oled.setRotation(ROTATION);
+  oled.setFont(gfxFont);
+  oled.setTextSize(TEXT_SIZE);
+  oled.display();
+  delay(2000);
+  oled.clearDisplay();
+  oled.display();
   uint8_t fontHeight = gfxFont->yAdvance;
-  Serial.print(F("Font height: "));
-  Serial.println(fontHeight);
+  uint8_t fontWidth = getTextWidth();
+  uint8_t maxRows = oled.height() / fontHeight;
+  uint8_t maxColumns = oled.width() / fontWidth;
+  Serial.print(F("Setup done: fontHeight|fontWidth|tftHeight|tftWidth|maxRows|maxColumns: "));
+  Serial.print(fontHeight);
+  Serial.print(F("|"));
+  Serial.print(fontWidth);
+  Serial.print(F("|"));
+  Serial.print(oled.height());
+  Serial.print(F("|"));
+  Serial.print(oled.width());
+  Serial.print(F("|"));
+  Serial.print(maxRows);
+  Serial.print(F("|"));
+  Serial.println(maxColumns);
+  oled.setCursor(0, fontHeight);
+  oled.setTextColor(WHITE);
+  oled.print("Testing");
+  oled.display();
 }
 
 void loop() {
 
+}
+
+uint8_t getTextWidth() {
+  int16_t x1, y1;
+  uint16_t w, h;
+  oled.getTextBounds("A", 0, 0, &x1, &y1, &w, &h);
+  return w;
 }
